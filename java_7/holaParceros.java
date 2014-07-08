fdcfcac588c05368d451244348d6f139387a7f11
Luis Benitez <luisbenitez02@gmail.com>
2014-07-08 17:55:05 -0500

    Primer programa
    el primer programa del curso de Video2Brain

---
+++ java_7/holaParceros.java
@@ -0,0 +1,5 @@
+class holaParceros{//clase igual a nombre de archivo .java
+	public static void main(String[] args) {//lo que es programa va a hacer
+		System.out.println("Q'hubo Parceros ¿bien o que?");
+	}
+}
